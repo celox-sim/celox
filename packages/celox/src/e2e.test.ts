cf6d034c454069ebafdb718d022909746bb291b1
tignear <tignear+m@gmail.com>
2026-04-12 08:09:12 +0000

    Adjust package e2e interface array source for latest veryl

--- packages/celox/src/e2e.test.ts
+++ packages/celox/src/e2e.test.ts
@@ -2105,7 +2105,7 @@ module Top (
     bus: modport Bus::consumer [2],
     out: output logic<8>,
 ) {
-    assign out = bus.data[0] + bus.data[1];
+    assign out = bus[0].data + bus[1].data;
 }
 `;
 
